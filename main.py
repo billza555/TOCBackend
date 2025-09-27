e815ac108d3559f0ebe436936a5d85bf59eea8da
billza555 <nphisu@gmail.com>
2025-09-27 12:50:14 +0700

    feat: allow cors

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ from fastapi import FastAPI
 import uvicorn
 from modules.songs.controller import router as song_router
 from core.config import Config
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create FastAPI app
 app = FastAPI(
     title="Music Scraper API",
     description="API for scraping songs, lyrics, and chord images",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],   
+    allow_headers=["*"],   
 )
 
 # Include routers
